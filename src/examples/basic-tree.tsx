c341b08d5e5747961e293511e70661e7913febb6
Grace H <gracefh@mit.edu>
2022-10-05 19:43:10 -0400

    add super basic flextree component

--- src/examples/basic-tree.tsx
+++ src/examples/basic-tree.tsx
@@ -49,8 +49,6 @@ export type LinkProps = {
 };
 
 export const Link = forwardRef(({ opId, start, end }: LinkProps, ref: any) => {
-  const linkRef = useRef(null);
-
   return (
     <Connector
       ref={ref}
@@ -59,7 +57,25 @@ export const Link = forwardRef(({ opId, start, end }: LinkProps, ref: any) => {
       $to={'topCenter'}
       stroke={'#000000'}
       strokeWidth={5}
-      strokeDasharray={3}
+      strokeDasharray={0}
+    >
+      <Ref to={start.opId} />
+      <Ref to={end.opId} />
+    </Connector>
+  );
+});
+
+// a link for a flex tree that goes horizontally from a start node to an end node
+export const FlexLink = forwardRef(({ opId, start, end }: LinkProps, ref: any) => {
+  return (
+    <Connector
+      ref={ref}
+      name={opId}
+      $from={'centerRight'}
+      $to={'centerLeft'}
+      stroke={'#000000'}
+      strokeWidth={5}
+      strokeDasharray={0}
     >
       <Ref to={start.opId} />
       <Ref to={end.opId} />
@@ -105,12 +121,6 @@ export const Tree = forwardRef(({ nodes, parentChild, levels }: TreeProps, ref: 
             </Row>
           </Group>
         ))}
-
-        <Align center>
-          <Ref to={`level0`} />
-          <Ref to={`level1`} />
-        </Align>
-
         <Space name={'space-levels'} vertically by={200}>
           <Ref to={'level0'} />
           <Ref to={'level1'} />
@@ -124,22 +134,69 @@ export const Tree = forwardRef(({ nodes, parentChild, levels }: TreeProps, ref: 
         {links.map((link) => (
           <Link {...link} />
         ))}
+      </Group>
+    </SVG>
+  );
+});
 
-        {/* not exactly sure why the connector from to doesn't work correctly */}
+export const FlexTree = forwardRef(({ nodes, parentChild, levels }: TreeProps, ref: any) => {
+  // const nodesRef = useRef(null);
+  const links = parentChild.map((pair, index) => {
+    return { opId: `link${index}`, start: { opId: pair.parent.opId }, end: { opId: pair.child.opId } };
+  });
+  const nodesMap: Map<string, NodeProps> = new Map();
 
-        {/* {links.map((link) => (
-          <Connector
-            name={link.opId}
-            $from={'bottomCenter'}
-            $to={'topCenter'}
-            stroke={'#000000'}
-            strokeWidth={5}
-            strokeDasharray={0}
-          >
-            <Ref to={link.start.opId} />
-            <Ref to={link.end.opId} />
-          </Connector>
+  nodes.forEach((node) => nodesMap.set(node.opId, node));
+
+  console.log('level keys: ', levels.keys());
+
+  return (
+    <SVG width={2000} height={2000}>
+      <Group>
+        {levels.map((level, index: number) => (
+          <Group name={`level${index}`}>
+            <Col name={`col${index}`} spacing={20} alignment={'center'}>
+              {level.nodes.map((node) => (
+                <Node {...nodesMap.get(node)!} />
+              ))}
+            </Col>
+          </Group>
+        ))}
+
+        {/* {Array.from(Array(levels.length - 1).keys()).map((level: number) => (
+          <Space name={`space-level-${level}`} horizontally by={200}>
+            <Ref to={`level${level + 1}`} />
+            <Ref to={`level${level}`} />
+          </Space>
         ))} */}
+
+        {/* extract this out into actual logic */}
+        <Space name={'space-levels-0'} horizontally by={600}>
+          <Ref to={'level0'} />
+          <Ref to={'level3'} />
+        </Space>
+        <Space name={'space-levels-1'} horizontally by={400}>
+          <Ref to={'level0'} />
+          <Ref to={'level2'} />
+        </Space>
+        <Space name={'space-levels-2'} horizontally by={200}>
+          <Ref to={'level0'} />
+          <Ref to={'level1'} />
+        </Space>
+
+        {/* <Space name={'space-levels-2'} horizontally by={200}>
+          <Ref to={'level3'} />
+          <Ref to={'level2'} />
+        </Space> */}
+
+        {/* <Col name={'nodes'} ref={nodesRef} spacing={20} alignment={'center'}>
+          {nodes.map((node) => (
+            <Node {...node} />
+          ))}
+        </Col> */}
+        {links.map((link) => (
+          <FlexLink {...link} />
+        ))}
       </Group>
     </SVG>
   );
