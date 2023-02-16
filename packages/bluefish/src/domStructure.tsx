61ac2bd655e80ec04cf6a413631454400f01751b
Catherine Mei <meic1212@mit.edu>
2023-02-16 16:21:51 -0500

    started building dom structure
    - need to fix sibling <a> relationship issue (ask jonathan)

---
+++ packages/bluefish/src/domStructure.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react';
+
+export function DomStructure() {
+  useEffect(() => {
+    // put timer for 1 second
+    setTimeout(() => {
+      const allRefs = Array.from(document.querySelectorAll('.ref'));
+      // for each ref in allRefs, get the data-to attribute
+
+      const allRefsTo = allRefs.map((ref) => ref.getAttribute('data-to'));
+      console.log(
+        'allRefs',
+        Array.from(allRefs).map((ref: any) => {
+          return ref.innerHTML;
+        }),
+      );
+      console.log(`allRefs`, allRefs);
+      console.log('allRefsTo', allRefsTo);
+      allRefsTo.forEach((refTo) => {
+        if (!refTo) {
+          return;
+        }
+        const domObject = window[refTo as any];
+        console.log('dom objects');
+        console.log(domObject);
+
+        if (domObject) {
+          const linkChild = document.createElementNS('http://www.w3.org/2000/svg', 'a');
+          linkChild.setAttribute('href', `#${refTo}`);
+          // create SVG text node
+          const textNode = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+          linkChild.appendChild(textNode);
+          linkChild.setAttribute('aria-label', `Link to ${refTo}`);
+          console.log(linkChild);
+          (domObject as any).appendChild(linkChild);
+
+          const squareChild = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+          squareChild.setAttribute('x', '0');
+          squareChild.setAttribute('y', '0');
+          squareChild.setAttribute('width', '100');
+          squareChild.setAttribute('height', '100');
+          squareChild.setAttribute('aria-label', `square`);
+          (domObject as any).appendChild(squareChild);
+        }
+
+        console.log(domObject);
+      });
+
+      console.log(allRefsTo);
+    }, 1000);
+  }, []);
+
+  return null;
+}
