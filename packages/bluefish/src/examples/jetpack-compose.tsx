b76ad21cbc3830857ff876b4ee4e595f317b7127
Grace H <gracefh@mit.edu>
2023-09-11 14:42:46 -0400

    finish initial implementation of jetpack compose example

--- packages/bluefish/src/examples/jetpack-compose.tsx
+++ packages/bluefish/src/examples/jetpack-compose.tsx
@@ -1,4 +1,5 @@
 import { useName, useNameList, withBluefish } from '../bluefish';
+import { Contain } from '../components/Contain';
 import { Distribute } from '../components/Distribute';
 import { Rect } from '../components/Rect';
 import { SVG } from '../components/SVG';
@@ -18,8 +19,9 @@ const ComposeText = withBluefish((props: { contents: string; fontSize?: string; 
   );
 });
 
+// Make expanded view as well
 const dayToSleepBars = [
-  { day: 'Sun', sleep: 7 },
+  { day: 'Sun', sleep: 7, selected: true },
   { day: 'Mon', sleep: 6.5 },
   { day: 'Tue', sleep: 8 },
   { day: 'Wed', sleep: 7 },
@@ -28,59 +30,121 @@ const dayToSleepBars = [
   { day: 'Sat', sleep: 9 },
 ];
 
+const displayedHours = [20, 21, 22, 23, 0, 1, 2];
+const displayedResolutions = [{ name: 'day' }, { name: 'week', selected: true }, { name: 'month' }, { name: '6M' }];
+
+// Scaffolding for basic perf testing
+const numEntries = 20;
+const dayToSleepBars2 = [...Array(numEntries)].map((i, ind) => ({ day: `day-${ind}`, sleep: Math.random() * 2 + 6 }));
+
 const sleepBarWidth = (sleepTime: number) => 50 * sleepTime;
+
+type ColorOffset = { offset: number; color: string };
+
+type GradientProps = {
+  id: string;
+  colorOffsets: ColorOffset[];
+  x1?: number;
+  x2?: number;
+  y1?: number;
+  y2?: number;
+};
+const Gradient = withBluefish((props: GradientProps) => {
+  return (
+    <g>
+      <linearGradient id={props.id} x1={props.x1} x2={props.x2} y1={props.y1} y2={props.y2}>
+        {props.colorOffsets.map((colorOffset) => (
+          <stop offset={`${colorOffset.offset}%`} stop-color={colorOffset.color} />
+        ))}
+      </linearGradient>
+    </g>
+  );
+});
 
 export const JetpackCompose: React.FC<{}> = withBluefish(() => {
   const resolution = useName('resolution');
   const hours = useName('hours');
   const sleepBarRows = useName('sleepBarRows');
   const sleepBars = useNameList(dayToSleepBars.map((item, i) => `sleepBar-${i}`));
+  const selectedResolution = useName('selectedResolution');
+  const selectedResolutionRect = useName('selectedResolutionRect');
+
+  const hoursBarLength = displayedHours.length * 70 + 10;
 
   return (
     <SVG width={1000} height={800}>
-      {/* <linearGradient id="sampleGradient">
-        <stop offset="0%" stop-color="#F1E9BC" />
-        <stop offset="100%" stop-color="#F2E3AC" />
-      </linearGradient> */}
+      <Gradient
+        id="sleepBarGradient"
+        colorOffsets={[
+          { offset: 0, color: '#F1E9BC' },
+          { offset: 100, color: '#f7d590' },
+        ]}
+        x1={0}
+        x2={0}
+        y1={0}
+        y2={1}
+      />
+      <Gradient
+        id="hoursBarGradient"
+        colorOffsets={[
+          { offset: 0, color: '#f8e7a0' },
+          { offset: 100, color: '#f8d183' },
+        ]}
+      />
       <Padding all={10}>
         <Group>
-          <Distribute direction="vertical" spacing={40}>
+          <Distribute direction="vertical" spacing={50}>
             <Row spacing={100} alignment="middle" name={resolution}>
-              <ComposeText contents="Day" fontSize="20" fill="#717171" />
-              <Group>
-                <Align alignment="center">
-                  <Rect width={80} height={30} fill="#fff" rx="10px" stroke="#EEDFAB" strokeWidth={2} />
-                  <ComposeText contents="Week" fontSize="20" />
-                </Align>
-              </Group>
-              <ComposeText contents="Month" fontSize="20" fill="#717171" />
-              <ComposeText contents="6M" fontSize="20" fill="#717171" />
+              {displayedResolutions.map((resolution) =>
+                resolution.selected === true ? (
+                  <Group name={selectedResolution}>
+                    <Align alignment="center">
+                      <Padding {...{ left: 20, right: 0, bottom: 2, top: 0 }}>
+                        {/* not a great fix but makes the alignment actually on center */}
+                        <Rect
+                          width={80}
+                          height={30}
+                          fill="#fff"
+                          rx="10px"
+                          stroke="#EEDFAB"
+                          strokeWidth={2}
+                          name={selectedResolutionRect}
+                        />
+                      </Padding>
+
+                      <Padding {...{ left: 0, right: 0, bottom: 2, top: 0 }}>
+                        <ComposeText contents={resolution.name} fontSize="20" />
+                      </Padding>
+                    </Align>
+                  </Group>
+                ) : (
+                  <ComposeText contents={resolution.name} fontSize="20" fill="#717171" />
+                ),
+              )}
             </Row>
             <Group>
+              {/* make this a contain */}
               <Align alignment="centerRight">
-                <Rect width={450} height={35} fill="#EFD489" rx="10px" />
-                <Padding name={hours} left={0} right={20} top={5} bottom={5}>
+                <Rect width={hoursBarLength} height={40} fill="url(#hoursBarGradient)" rx="10px" />
+                <Padding name={hours} left={0} right={50} top={5} bottom={5}>
                   <Row spacing={60} alignment="middle">
-                    <ComposeText contents="20" />
-                    <ComposeText contents="21" />
-                    <ComposeText contents="22" />
-                    <ComposeText contents="23" />
-                    <ComposeText contents="0" />
-                    <ComposeText contents="1" />
-                    <ComposeText contents="2" />
+                    {displayedHours.map((hour) => (
+                      <ComposeText contents={hour.toString()} />
+                    ))}
                   </Row>
                 </Padding>
               </Align>
             </Group>
 
+            {/* Make a sort of plot where the rectangles scale */}
             <Col spacing={20} alignment="center" name={sleepBarRows}>
               {dayToSleepBars.map((dayToSleepBar, ind) => (
-                <Row alignment="middle" totalWidth={600}>
+                <Row alignment={dayToSleepBar.selected === true ? 'top' : 'middle'} totalWidth={hoursBarLength + 80}>
                   <ComposeText contents={dayToSleepBar.day} />
                   <Rect
                     width={sleepBarWidth(dayToSleepBar.sleep)}
-                    height={30}
-                    fill="#F1E9BC"
+                    height={dayToSleepBar.selected === true ? 80 : 30}
+                    fill="url(#sleepBarGradient)"
                     rx="10px"
                     name={sleepBars[ind]}
                   />
