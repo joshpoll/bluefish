f54f992a81ddfccf032a87b5c9cbc4dd582d03f6
Catherine Mei <meic1212@mit.edu>
2023-09-14 23:37:36 -0400

    cleaned up code

--- packages/bluefish/src/examples/python-tutor/Objects.tsx
+++ packages/bluefish/src/examples/python-tutor/Objects.tsx
@@ -13,32 +13,28 @@ export type ObjectProps = {
   objectId: string;
 };
 
-export const Objects = withBluefish(function ({ objectType, objectValues, objectId }: any) {
-  // objectValues: list of values for different parts of object
-
-  const objectTypeRef = useName('objectTypeLabel');
-  const objectRef = useName('objectGroup');
+export const Objects = withBluefish(function ({ objectType, objectValues, id }: any) {
+  const objectTypeLabel = useName('objectTypeLabel');
+  const objects = useName('objects');
 
   const fontFamily = 'verdana, arial, helvetica, sans-serif';
 
-  //console.log('creating an object with id: ', objectId, objectType, objectValues);
-
   return (
-    <Group name={objectId}>
-      <Text name={objectTypeRef} contents={objectType} fontFamily={fontFamily} fontSize={'16px'} fill={'grey'} />
-      <Row name={objectRef} spacing={0} alignment={'middle'}>
-        {objectValues.map((elementData: any, index: any) => (
-          <ElmTuple tupleIndex={index} tupleData={elementData} objectId={objectId} />
+    <Group name={id}>
+      <Text name={objectTypeLabel} contents={objectType} fontFamily={fontFamily} fontSize={'16px'} fill={'grey'} />
+      <Row name={objects} spacing={0} alignment={'middle'}>
+        {objectValues.map((tupleData: any, index: any) => (
+          <ElmTuple tupleIndex={index} tupleData={tupleData} parentObject={id} />
         ))}
       </Row>
 
       <Distribute direction={'vertical'} spacing={10}>
-        <Ref to={objectTypeRef} />
-        <Ref to={objectRef} />
+        <Ref to={objectTypeLabel} />
+        <Ref to={objects} />
       </Distribute>
       <Align alignment={'left'}>
-        <Ref to={objectTypeRef} />
-        <Ref to={objectRef} />
+        <Ref to={objectTypeLabel} />
+        <Ref to={objects} />
       </Align>
     </Group>
   );
