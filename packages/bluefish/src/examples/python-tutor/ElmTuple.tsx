18161f0a91fd972289d7845fc7ec18a288930760
Catherine Mei <meic1212@mit.edu>
2023-09-14 23:54:37 -0400

    cleaned up code for python tutor

--- packages/bluefish/src/examples/python-tutor/ElmTuple.tsx
+++ packages/bluefish/src/examples/python-tutor/ElmTuple.tsx
@@ -9,43 +9,42 @@ import { Padding } from '../../main';
 export type ElmTupleProps = {
   tupleIndex: string;
   tupleData: { type: string; value: string };
-  objectId: string;
+  parentObject: string;
 };
 
-export const ElmTuple = withBluefish(({ tupleIndex, tupleData, objectId }: ElmTupleProps) => {
+export const ElmTuple = withBluefish(({ tupleIndex, tupleData, parentObject }: ElmTupleProps) => {
   const fontFamily = 'verdana, arial, helvetica, sans-serif';
   return (
-    <Group name={`elm_${tupleIndex}_${objectId}` as any}>
-      <Rect name={`elmBox_${tupleIndex}_${objectId}` as any} height={60} width={70} fill={'#ffffc6'} stroke={'grey'} />
-      <Padding left={5} right={0} top={0} bottom={0} name={`elmLabel_${tupleIndex}_${objectId}` as any}>
-        <Text
-          // name={`elmLabel_${tupleIndex}_${objectId}` as any}
-          contents={`${tupleIndex}`}
-          fontFamily={fontFamily}
-          fontSize={'16px'}
-          fill={'gray'}
-        />
-        {/* <Rect name={`elmLabel_${tupleIndex}_${objectId}` as any} height={10} width={10} fill={'none'} stroke={'red'} /> */}
+    <Group name={`${parentObject}_tuple${tupleIndex}` as any}>
+      <Rect
+        name={`${parentObject}_tuple${tupleIndex}_box` as any}
+        height={60}
+        width={70}
+        fill={'#ffffc6'}
+        stroke={'grey'}
+      />
+      <Padding left={5} right={0} top={0} bottom={0} name={`${parentObject}_tuple${tupleIndex}_label` as any}>
+        <Text contents={`${tupleIndex}`} fontFamily={fontFamily} fontSize={'16px'} fill={'gray'} />
       </Padding>
-      {tupleData.type == 'string' ? (
+      {tupleData.type === 'string' ? (
         <Text
-          name={`elmVal_${tupleIndex}_${objectId}` as any}
+          name={`${parentObject}_tuple${tupleIndex}_value` as any}
           contents={tupleData.value}
           fontSize={'24px'}
           fill={'black'}
         />
       ) : (
-        <Text name={`elmVal_${tupleIndex}_${objectId}` as any} contents={''} fill={'none'} />
+        <Text name={`${parentObject}_tuple${tupleIndex}_value` as any} contents={''} fill={'none'} />
       )}
 
       <Align alignment="center">
-        <Ref to={`elmVal_${tupleIndex}_${objectId}`} />
-        <Ref to={`elmBox_${tupleIndex}_${objectId}`} />
+        <Ref to={`${parentObject}_tuple${tupleIndex}_value`} />
+        <Ref to={`${parentObject}_tuple${tupleIndex}_box`} />
       </Align>
 
       <Align alignment="topLeft">
-        <Ref to={`elmLabel_${tupleIndex}_${objectId}`} />
-        <Ref to={`elmBox_${tupleIndex}_${objectId}`} />
+        <Ref to={`${parentObject}_tuple${tupleIndex}_label`} />
+        <Ref to={`${parentObject}_tuple${tupleIndex}_box`} />
       </Align>
     </Group>
   );
